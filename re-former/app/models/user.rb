fa9794d8ce848c865d6b06ac129aa346b9eff0f7
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-02-13 16:43:33 +0100

    Create HTML Form

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, length: { minimum: 4, maximum: 15 }, uniqueness: true
-  validates :emails, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6, maximum: 20 }
-
+  validates :name, :email, :password, presence: true
 end
--- re-former/db/schema.rb
+++ re-former/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213130541) do
+ActiveRecord::Schema.define(version: 20160213145306) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
     t.string   "password"
     t.datetime "created_at", null: false
